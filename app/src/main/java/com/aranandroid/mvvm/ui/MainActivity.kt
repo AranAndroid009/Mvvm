a17e45d010ca8aaeb77036438781e2f84800ed79
shuangshuang.qin <shuangshuang.qin@winspread.com>
2021-05-06 11:55:14 +0800

    ces

--- app/src/main/java/com/aranandroid/mvvm/ui/MainActivity.kt
+++ app/src/main/java/com/aranandroid/mvvm/ui/MainActivity.kt
@@ -11,8 +11,6 @@ import com.aranandroid.mvvm.ui.area.ChooseAreaFragment
 
 class MainActivity : BaseActivity<MainViewModel, ActivityMainBinding>(R.layout.activity_main) {
 
-
-    //测试
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         if (viewModel.isWeatherCached()) {
